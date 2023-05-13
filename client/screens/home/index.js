9d9e33b4ffd34d0d28e7898162ccab342b01b723
ling <sun_wling@163.com>
2023-05-13 19:42:18 +0800

    integrete @emotion/styled

--- client/screens/home/index.js
+++ client/screens/home/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { Link } from '@reach/router'
+import styled from "@emotion/styled"
+
+const Title = styled.div`
+  font-size: 20px;
+`
 
 const Home = () => {
   return (
     <>
-      <div>home page</div>
+      <Title>home page</Title>
       <Link to="/money">link to money page</Link>
       <div>hello world</div>
     </>
